9f6f260fda42c7b4cef2866344a42f756785ebc9
Jinewick <jwickhernal@gmail.com>
2025-02-10 13:35:32 +0800

    Fist Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>TALAVERA - WEB</title>
+  <title>WEB LARAVEL</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
@@ -61,7 +61,7 @@
       <div class="container">
         <div class="row gy-4">
           <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-            <h1 data-aos="fade-up">We offer modern solutions for growing your business</h1>
+            <h1 data-aos="fade-up">agasgagag</h1>
             <p data-aos="fade-up" data-aos-delay="100">We are team of talented designers making websites with Bootstrap</p>
             <div class="d-flex flex-column flex-md-row" data-aos="fade-up" data-aos-delay="200">
               <a href="#about" class="btn-get-started">Get Started <i class="bi bi-arrow-right"></i></a>
